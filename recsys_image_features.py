0ec617b30af86ab7ad7083e60b510aee3d2613db
Kanishtha Siyaram <69192294+kanishths@users.noreply.github.com>
2024-03-15 18:09:12 +0530

    Update recsys_image_features.py

--- recsys_image_features.py
+++ recsys_image_features.py
@@ -1,3 +1,97 @@
+# import streamlit as st
+# from zipfile import ZipFile
+# import os
+# import glob
+# from PIL import Image
+# import matplotlib.pyplot as plt
+# from tensorflow.keras.preprocessing import image
+# from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+# from tensorflow.keras.models import Model
+# import numpy as np
+# from scipy.spatial.distance import cosine
+
+# st.cache_data
+# def extract_features_and_paths(data_path):
+#     extraction_dir = 'women-fashion'
+#     if not os.path.exists(extraction_dir):
+#         os.makedirs(extraction_dir)
+
+#     with ZipFile(data_path, 'r') as zip_ref:
+#         zip_ref.extractall(extraction_dir)
+
+#     image_directory = os.path.join(extraction_dir, 'women-fashion')
+#     # print(image_directory)
+
+#     image_paths_list = [file for file in glob.glob(os.path.join(image_directory, '*.*')) if file.endswith(('.jpg', '.png', '.jpeg', 'webp'))]
+
+#     base_model = ResNet50(weights='imagenet', include_top=False)
+#     model = Model(inputs=base_model.input, outputs=base_model.output)
+
+#     all_features = []
+#     all_image_names = []
+
+#     for img_path in image_paths_list:
+#         preprocessed_img = preprocess_image(img_path)
+#         features = extract_features(model, preprocessed_img)
+#         all_features.append(features)
+#         all_image_names.append(os.path.basename(img_path))
+#     # for feat in all_image_names:
+#     #         print(feat)
+#     return all_features, all_image_names, model, image_paths_list
+
+# def preprocess_image(img_path):
+#     img = image.load_img(img_path, target_size=(224, 224))
+#     img_array = image.img_to_array(img)
+#     img_array_expanded = np.expand_dims(img_array, axis=0)
+#     return preprocess_input(img_array_expanded)
+
+# def extract_features(model, preprocessed_img):
+#     features = model.predict(preprocessed_img)
+#     flattened_features = features.flatten()
+#     normalized_features = flattened_features / np.linalg.norm(flattened_features)
+#     return normalized_features
+
+# def recommend_fashion_items_resnet(input_image_path, all_features, all_image_names, model, top_n=5):
+#     preprocessed_img = preprocess_image(input_image_path)
+#     input_features = extract_features(model, preprocessed_img)
+
+#     similarities = [1 - cosine(input_features, other_feature) for other_feature in all_features]
+#     similar_indices = np.argsort(similarities)[-top_n:]
+#     print(input_image_path)
+#     print("\n")
+#     for i in all_image_names:
+#         print(i)
+#     similar_indices = [idx for idx in similar_indices if idx != all_image_names.index(input_image_path)]
+
+#     st.image(input_image_path, caption="Input Image", use_column_width=True)
+
+#     for i, idx in enumerate(similar_indices[:top_n], start=1):
+#         # image_path = os.path.join('', all_image_names[idx])
+#         image_path = all_image_names[idx]
+#         st.image(image_path, caption=f"Recommendation {i}", use_column_width=True)
+
+# def main():
+#     st.title("Fashion Item Recommender")
+
+#     uploaded_file = st.file_uploader("Upload a zip file", type="zip")
+#     if uploaded_file:
+#         data_path = 'uploaded.zip'
+#         with open(data_path, "wb") as f:
+#             f.write(uploaded_file.getbuffer())
+
+#         all_features, all_image_names, model, image_paths_list = extract_features_and_paths(data_path)
+
+#         st.write("Select an image for recommendation:")
+#         selected_image_path = st.selectbox("Select an image", image_paths_list)
+#         input_image_path = selected_image_path
+#         pathh = input_image_path[13:]
+#         print(pathh)
+#         recommend_fashion_items_resnet(pathh, all_features, all_image_names, model)
+
+# if __name__ == "__main__":
+#     main()
+
+
 import streamlit as st
 from zipfile import ZipFile
 import os
@@ -10,7 +104,6 @@ from tensorflow.keras.models import Model
 import numpy as np
 from scipy.spatial.distance import cosine
 
-st.cache_data
 def extract_features_and_paths(data_path):
     extraction_dir = 'women-fashion'
     if not os.path.exists(extraction_dir):
@@ -23,6 +116,9 @@ def extract_features_and_paths(data_path):
     # print(image_directory)
 
     image_paths_list = [file for file in glob.glob(os.path.join(image_directory, '*.*')) if file.endswith(('.jpg', '.png', '.jpeg', 'webp'))]
+
+    for i in image_paths_list:
+      print(i)
 
     base_model = ResNet50(weights='imagenet', include_top=False)
     model = Model(inputs=base_model.input, outputs=base_model.output)
@@ -57,10 +153,10 @@ def recommend_fashion_items_resnet(input_image_path, all_features, all_image_nam
 
     similarities = [1 - cosine(input_features, other_feature) for other_feature in all_features]
     similar_indices = np.argsort(similarities)[-top_n:]
-    print(input_image_path)
-    print("\n")
-    for i in all_image_names:
-        print(i)
+    # print(input_image_path)
+    # print("\n")
+    # for i in all_image_names:
+    #     print(i)
     similar_indices = [idx for idx in similar_indices if idx != all_image_names.index(input_image_path)]
 
     st.image(input_image_path, caption="Input Image", use_column_width=True)
@@ -83,7 +179,7 @@ def main():
         st.write("Select an image for recommendation:")
         selected_image_path = st.selectbox("Select an image", image_paths_list)
         input_image_path = selected_image_path
-        pathh = input_image_path#[26:]
+        pathh = input_image_path#[13:]
         print(pathh)
         recommend_fashion_items_resnet(pathh, all_features, all_image_names, model)
 
