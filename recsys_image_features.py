e2347119d9131bd127d829a5d61d6a16a552c334
Kanishtha Siyaram <69192294+kanishths@users.noreply.github.com>
2024-03-15 19:42:43 +0530

    Update recsys_image_features.py

--- recsys_image_features.py
+++ recsys_image_features.py
@@ -79,9 +79,17 @@ def main():
         input_features, all_features, all_image_names =  dataset_creation(data_path)
         st.write("Dataset Created with ",len(all_image_names)," images")
         st.write("Select an image for recommendation:")
-        selected_image_path = st.selectbox("Select an image", all_image_names, on_change=lambda x: results(x, input_features, all_features, all_image_names))
-        input_image_path = selected_image_path
-        results(input_image_path, input_features, all_features, all_image_names)
+        # selected_image_path = st.selectbox("Select an image", all_image_names, on_change=lambda x: results(x, input_features, all_features, all_image_names))
+        # input_image_path = selected_image_path
+        # results(input_image_path, input_features, all_features, all_image_names)
+        selected_image_path = st.selectbox("Select an image", all_image_names, on_change=lambda new_image_path: update_selected_image(new_image_path))
+        
+        def update_selected_image(new_image_path):
+            global selected_image_path
+            selected_image_path = new_image_path
+            results(selected_image_path, input_features, all_features, all_image_names)
+        
+        results(selected_image_path, input_features, all_features, all_image_names)
 
 
         # result = recsys(input_image_path, input_features, all_features, all_image_names)
