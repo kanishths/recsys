ec4fd6d0346af796a43d562531092721581f3876
Kanishtha Siyaram <69192294+kanishths@users.noreply.github.com>
2024-03-15 19:36:57 +0530

    Update recsys_image_features.py

--- recsys_image_features.py
+++ recsys_image_features.py
@@ -79,7 +79,7 @@ def main():
         input_features, all_features, all_image_names =  dataset_creation(data_path)
         st.write("Dataset Created with ",len(all_image_names)," images")
         st.write("Select an image for recommendation:")
-        selected_image_path = st.selectbox("Select an image", all_image_names, on_change=results(input_image_path, input_features, all_features, all_image_names))
+        selected_image_path = st.selectbox("Select an image", all_image_names, on_change=lambda x: results(selected_image_path, input_features, all_features, all_image_names))
         input_image_path = selected_image_path
         results(input_image_path, input_features, all_features, all_image_names)
 
